829d27a46a755e496973f08db665ffce6f5d10e6
admstud <admstud@MX1-Docker-21.EDU-WSB.local>
2026-06-21 17:27:31 +0200

    porpawki błędów

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,15 +6,15 @@ from flask import request
 moje_imie = "Marek"
 msg = "Hello World!"
 
+
 @app.route('/')
-
-
 def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
